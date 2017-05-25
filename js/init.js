6aa8f5ab5280d1b520eca53f86cfed0aadead1ae
Alan Felix <alan.fncc@outlook.com.br>
2017-05-25 02:19:07 -0300

    CMT#005
    Implementação da Dashboard.
    Adição do arquivo init.js.
    Adição da página de erro (404.html).

---
+++ js/init.js
@@ -0,0 +1,40 @@
+function loginStart() {
+	var modal = document.getElementsByClassName('modal-loader')[0];
+	modal.classList.add('loading');
+	setTimeout(function(){
+		window.location.href = 'dashboardTela.html';
+	}, 900);
+}
+
+function loginEnd() {
+	var modal = document.getElementsByClassName('modal-loader')[0];
+	modal.classList.add('loading');
+	setTimeout(function(){
+		window.location.href = 'loginTela.html';
+	}, 900);
+}
+
+function showClientData() {
+	var clientId = document.getElementById('clientId');
+	if (clientId !== null || clientId !== "" || clientId !== undefined) {
+		document.getElementsByClassName('consulta-cliente')[0].classList.add('show');
+		document.getElementsByClassName('cadastro-cliente')[0].classList.remove('show');
+	}
+}
+
+function hideClientData() {
+	document.getElementsByClassName('consulta-cliente')[0].classList.remove('show');
+	document.getElementsByClassName('cadastro-cliente')[0].classList.add('show');
+}
+
+function cleanAllFields() {
+	var inputArea = document.getElementsByClassName('cadastro-cliente')[0];
+	var inputFields = inputArea.getElementsByTagName('input');
+	for (i=0; i < inputFields.length; i++) {
+		inputFields[i].value = "";
+	}
+}
+
+function forOfor() {
+	window.location.href = "404.html";
+}
